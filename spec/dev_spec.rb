4088fe5e1f570235e53de9620d52fa066f22b6e5
lou-parslow <lou.parslow@gmail.com>
2015-08-21 09:38:40 -0600

    'all'

--- spec/dev_spec.rb
+++ spec/dev_spec.rb
@@ -1,6 +1,24 @@
 require_relative('../lib/dev.rb')
 
 describe Dev do
+
+    it "should be able to make HelloRubyGem" do
+        dir="#{File.dirname(__FILE__)}/dev_spec_HelloRubyGem"
+        `git clone http://github.com/dev-gem/HelloRubyGem.git #{dir}`
+        Dir.chdir(dir) do
+            FileUtils.rm_r '.git'
+            Text.replace_in_file('rakefile.rb',"require 'dev'","require_relative('../../lib/dev.rb')")
+            rake_default=Command.new({:input=>'rake default',:ignore_failure=> true, :quiet => true})
+            rake_default.execute
+            if(rake_default[:exit_code] != 0)
+                puts rake_default[:output]
+                puts rake_default[:error]
+            end
+            expect(Command.exit_code('rake default')).to eq(0)
+        end
+        FileUtils.rm_r dir
+    end
+
     it "should be able to define rake tasks for the gem-example" do
         dir="#{File.dirname(__FILE__)}/dev_spec/hello.gem"
         FileUtils.rm_r dir if File.exists? dir
@@ -22,7 +40,7 @@ describe Dev do
         FileUtils.rm_r "#{File.dirname(__FILE__)}/dev_spec"
     end
 
-	it "should be able to define rake tasks for HelloLibrary" do
+	it "should be able to make HelloCSharpLibrary" do
     	#dir="#{File.dirname(__FILE__)}/dev_spec/HelloLibrary"
     	#FileUtils.mkdir_p dir if !File.exists?(dir)
     	#{}`git clone https://gitlab.com/lou-parslow/HelloLibrary.git #{dir}`
