11bfe67b203137aa528c9ce51a49d31f33081db1
lou-parslow <lou.parslow@gmail.com>
2015-08-26 11:56:56 -0600

    'all'

--- lib/tasks/clobber.rb
+++ lib/tasks/clobber.rb
@@ -8,9 +8,10 @@ CLOBBER.include('**/*.gem')
 # CLOBBER Folders
 CLOBBER.include('bin') if File.exists?('bin')
 CLOBBER.include('doc') if File.exists?('doc')
+CLOBBER.include('obj') if File.exists?('obj')
+CLOBBER.include('packages') if File.exists?('packages')
 
 CLOBBER.include('*.gem')
-CLOBBER.include('bin','obj','packages')
 
 desc 'performs clobber commands'
 task :clobber => [:clean] do Tasks.execute_task :clobber;end
