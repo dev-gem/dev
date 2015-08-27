59acb7bf37ff0567eb4d97021593ef41b688d881
Lou Parslow <lou.parslow@gmail.com>
2015-08-26 20:53:03 -0600

    'all'

--- lib/base/dir.rb
+++ lib/base/dir.rb
@@ -3,6 +3,9 @@ puts __FILE__ if defined?(DEBUG)
 require 'fileutils'
 
 class Dir
+  def self.make directory
+  	FileUtils.mkdir_p directory if !File.exists? directory
+  end
   def self.remove directory
     if(File.exists?(directory))
       begin
