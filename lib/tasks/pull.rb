68dba02d8c3856e7d5b4d6dd123916d6642d9f2c
Lou Parslow <lou.parslow@gmail.com>
2015-08-28 14:10:40 -0600

    'all'

--- lib/tasks/pull.rb
+++ lib/tasks/pull.rb
@@ -8,7 +8,7 @@ class Pull < Array
 	def update
 		if(Internet.available?)
 			if(File.exists?('.git') && `git config --list`.include?('user.name='))
-				add_quiet('git pull') if Git.branch == '* master'
+				add_quiet('git pull') if Git.branch == 'master'
 			end
 		end
 	end
