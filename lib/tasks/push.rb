ccaeb954c0d044853e788c1cf298f206277559f7
Lou Parslow <lou.parslow@gmail.com>
2015-08-28 14:12:40 -0600

    'all'

--- lib/tasks/push.rb
+++ lib/tasks/push.rb
@@ -6,8 +6,7 @@ task :push do Tasks.execute_task :push;end
 class Push < Array
 	def update
 		if(File.exists?('.git') && `git config --list`.include?('user.name=') && `git branch`.include?('* master'))
-			#self <<  'git push'
-			#self <<  'git push --tags'
+			add_quiet 'git push'
 			add_quiet 'git push --tags'
 		end
 	end
