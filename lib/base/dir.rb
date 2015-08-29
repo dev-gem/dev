3db6113d7cbedb2336a6b26d8207de88c34f2be9
Lou Parslow <lou.parslow@gmail.com>
2015-08-29 10:03:54 -0600

    'all'

--- lib/base/dir.rb
+++ lib/base/dir.rb
@@ -1,5 +1,3 @@
-puts __FILE__ if defined?(DEBUG)
-
 require 'fileutils'
 
 class Dir
@@ -25,18 +23,4 @@ class Dir
     puts "directory #{directory} is not empty"
     false
   end
-  #def self.remove_empty directory, recursive=false
-  #  if(File.exists?(directory))
-  #    if(recursive)
-  #      Dir.chdir(directory) do
-  #        Dir.glob('*').select {|f| File.directory? f}.each{|d|
-  #          Dir.remove_empty(d,true)
-  #          remove d if (Dir.entries(d) - %w{ . .. }).empty?
-  #        }
-  #      end
-  #    end
-
-  #    remove directory if (Dir.entries(directory) - %w{ . .. }).empty?
-  #  end
-  #end
 end
