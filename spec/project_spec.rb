7d8e7d54e0f64c29037f940fe6abd475b602da87
Lou Parslow <lou.parslow@gmail.com>
2015-08-29 10:19:41 -0600

    'all'

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -35,7 +35,6 @@ describe Project do
         # CLOBBER
         expect(helloRake.clobber.exit_code).to eq(0)
         expect(File.exists?(helloRake.wrk_dir)).to eq(false)
-        #expect(Dir.empty?(File.dirname(helloRake.wrk_dir))).to eq(true)
         expect(File.exists?(File.dirname(helloRake.wrk_dir))).to eq(false)
 
         Dir.remove dir
