36a32c3dfcb7ebd9bb5f832e249b584148c56304
Lou Parslow <lou.parslow@gmail.com>
2015-09-27 12:03:59 -0600

    'all'

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -27,6 +27,8 @@ describe Project do
             cmd=Command.execute("git clone #{dir}/HelloRake.git")
             Dir.chdir("#{dir}/HelloRake") do
                 File.open('rakefile.rb','w'){|f|f.puts 'task :default do; puts "ok"; end'}
+                cmd=Command.execute('git config user.email "lou-parslow+dev.gem@gamail.com"') if Git.user_email.length < 1
+                cmd=Command.execute('git config user.name "lou-parslow"') if Git.user_name.length < 1
                 cmd=Command.execute('git add rakefile.rb')
                 cmd=Command.execute('git commit -m"added rakefile.rb"')
                 cmd=Command.execute('git tag 0.0.0 -m"0.0.0"')
