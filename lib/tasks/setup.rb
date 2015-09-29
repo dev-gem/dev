6af181f9a48b82e278fb41069cfad4546bb1244b
lou-parslow <lou.parslow@gmail.com>
2015-09-29 16:41:45 -0600

    'all'

--- lib/tasks/setup.rb
+++ lib/tasks/setup.rb
@@ -99,17 +99,6 @@ class Setup < Array
 				  end
 			    end
 			}
-			Dir.glob('*.wxs').each{|wxs|
-				current_version=IO.read(wxs).scan(/Version=[\"']([\d.]+)[\"']/)[0][0]
-				puts "#{wxs} current version=#{current_version}" if env.debug?
-				if(current_version.include?('Version='))
-					target_version="Version=\"#{VERSION}\")="
-					if(current_version != target_version)
-						add_quiet "<%Text.replace_in_file('#{wxs}','#{current_version}','#{target_version}')%>"
-						#add_quiet "<%Text.replace_in_file('Value=\"#{current_version}\"','Value=\"#{target_version}\"')%>"
-					end
-				end
-			}
 			Dir.glob('**/*.wxs').each{|wxs|
 				current_version=IO.read(wxs).scan(/Version=[\"']([\d.]+)[\"']/)[0][0]
 				puts "#{wxs} current version=#{current_version}" if env.debug?
@@ -117,7 +106,6 @@ class Setup < Array
 					target_version=VERSION#{}"Version=\"#{VERSION}\")="
 					if(current_version != target_version)
 						add_quiet "<%Text.replace_in_file('#{wxs}','#{current_version}','#{target_version}')%>"
-						#add_quiet "<%Text.replace_in_file('Value=\"#{current_version}\"','Value=\"#{target_version}\"')%>"
 					end
 				end
 			}
