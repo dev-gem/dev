f5d8111598502efe34844a25da9adaa25dd08459
Lou Parslow <lou.parslow@gmail.com>
2015-10-20 18:17:06 -0600

    'all'

--- lib/tasks/clean.rb
+++ lib/tasks/clean.rb
@@ -2,7 +2,7 @@ puts __FILE__ if defined?(DEBUG)
 
 require 'rake/clean'
 # Clean Files
-CLEAN.include('**/*.{sdf,sud,ncb,cache,user,wixobj,wixpdb}')
+CLEAN.include('**/*.{sdf,sud,ncb,cache,user,wixobj,wixpdb,nupkg}')
 
 # Clean Folders
 CLEAN.include('obj') if File.exists?('obj')
