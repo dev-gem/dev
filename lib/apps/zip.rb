31790dc627dbe1999f5600f74a997dabf254eae1
kevin-marshall <kevin@marshallville.net>
2017-12-04 15:07:28 -0700

    'all'

--- lib/apps/zip.rb
+++ lib/apps/zip.rb
@@ -9,7 +9,7 @@ module Zip
   # zip_file full path to a zip file to be exported
   # destination directory where the zip file contents are to be placed
   def self.export zip_file, destination
-    raise "#{zip_file} does not exist." unless(::File.exists?(zip_file))
+    raise "#{zip_file} does not exist." unless(IO::File.exists?(zip_file))
     unzip(zip_file, destiation) unless(Dir.exists?(destination))
   end
 
@@ -20,11 +20,11 @@ module Zip
   # source_glob defaults to '**/*' to publish all files in the source_dir
   def self.publish destination, source_dir, source_filelist=FileList.new('**/*')
     Dir.mktmpdir do |dir|
-      tmp_file_name = "#{dir}/#{::File.basename(destination)}"
+      tmp_file_name = "#{dir}/#{IO::File.basename(destination)}"
       
       zip(source_dir, source_filelist, tmp_file_name)
       
-      destination_dir = ::File.dirname(destination)
+      destination_dir = IO::File.dirname(destination)
       FileUtils.mkpath(destination_dir) unless(Dir.exists?(destination_dir))
       
       FileUtils.cp(tmp_file_name, destination)
@@ -33,11 +33,11 @@ module Zip
 
   private
   def self.zip(base_directory, files_to_archive, zip_file) 
-    FileUtils.mkpath(::File.dirname(zip_file)) unless(Dir.exists?(::File.dirname(zip_file)))
+    FileUtils.mkpath(IO::File.dirname(zip_file)) unless(Dir.exists?(IO::File.dirname(zip_file)))
     io = Zip::File.open(zip_file, Zip::File::CREATE); 
   
     files_to_archive.each do |file|
-      io.get_output_stream(file) { |f| f.puts(File.open("#{base_directory}/#{file}", "rb").read())} 
+      io.get_output_stream(file) { |f| f.puts(IO::File.open("#{base_directory}/#{file}", "rb").read())} 
     end
   
     io.close(); 
