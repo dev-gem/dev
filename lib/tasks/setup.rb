ff00c622c3d273b104a7d6917d079f7899fc78f7
kevin-marshall <kevin@marshallville.net>
2017-12-04 18:04:26 -0700

    'all'

--- lib/tasks/setup.rb
+++ lib/tasks/setup.rb
@@ -71,8 +71,10 @@ class Setup < Array
 			}
 		end
 
+		puts "Here"
 		if(defined?(ZIP_EXPORTS))
 			ZIP_EXPORTS.each{|k,v|
+			    puts "Here: #{k}"
 				directory = "#{Command.dev_root}/dep/#{k}"
 				Zip.export(v, k) unless(Dir.exists?(directory))
 			}
