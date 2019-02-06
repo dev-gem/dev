c724dd0bfb3d3ffb2aedeff4bd09f2cebe076d05
lparslow <lou.parslow@gmail.com>
2019-02-06 15:03:52 -0700

    'all'

--- dev.gemspec
+++ dev.gemspec
@@ -15,7 +15,7 @@
 #
 Gem::Specification.new do |s|
 	s.name          = 'dev'
-	s.version		= '2.1.105'
+	s.version		= '2.1.106'
 	s.date			= '2019-02-06'
 	s.summary		= 'dev'
 	s.description	= 'development tasks'
---
+++ lib/base/giturl.rb
@@ -0,0 +1,32 @@
+class GitUrl
+    def self.build url 
+
+        if(url.kind_of?(Array))
+            puts "url is an Array"
+            url.each{|u| GitUrl.build u}
+        else
+            puts "build #{url}"
+            work_dir = get_work_dir(url)
+            puts "work_dir #{work_dir}"
+            if(!Dir.exists?(work_dir))
+                puts "git clone #{url} #{work_dir}"
+                puts `git clone #{url} #{work_dir}`
+            end
+        
+            Dir.chdir(work_dir) do
+                puts "git pull (#{work_dir})"
+                puts `git pull`
+                puts "rake #{work_dir}"
+                puts `rake`
+            end
+        end
+    end
+    
+    def self.get_work_dir url
+        Environment.dev_root + "/work/" + get_relative_dir(url)
+    end
+    
+    def self.get_relative_dir url 
+        url.gsub('http://','').gsub('http://','').gsub('.git','')
+    end
+end
