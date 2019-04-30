6e034c55da788c82486b199ef58a26e1715d47a4
lou-parslow <lou.parslow@gmail.com>
2019-04-30 09:02:56 -0600

    'all'

--- dev.gemspec
+++ dev.gemspec
@@ -16,7 +16,7 @@
 Gem::Specification.new do |s|
 	s.name          = 'dev'
 	s.version		= '2.1.131'
-	s.date			= '2019-04-22'
+	s.date			= '2019-04-30'
 	s.summary		= 'dev'
 	s.description	= 'development tasks'
 	s.authors		= ["Lou Parslow"]
--- lib/base/giturl.rb
+++ lib/base/giturl.rb
@@ -1,10 +1,10 @@
 class GitUrl
-    def self.build url 
 
+    def self.pull url
         if(url.kind_of?(Array))
             url.each{|u| GitUrl.build u}
         else
-            puts "build #{url}"
+            puts "pull #{url}"
             work_dir = get_work_dir(url)
             puts "work_dir #{work_dir}"
             if(!Dir.exists?(work_dir))
@@ -15,6 +15,27 @@ class GitUrl
             Dir.chdir(work_dir) do
                 puts "git pull (#{work_dir})"
                 puts `git pull`
+            end
+        end
+    end
+
+    def self.build url 
+
+        if(url.kind_of?(Array))
+            url.each{|u| GitUrl.build u}
+        else
+            GitUrl.pull url
+            puts "build #{url}"
+            work_dir = get_work_dir(url)
+            puts "work_dir #{work_dir}"
+            #if(!Dir.exists?(work_dir))
+            #    puts "git clone #{url} #{work_dir}"
+            #    puts `git clone #{url} #{work_dir}`
+            #end
+        
+            Dir.chdir(work_dir) do
+                #puts "git pull (#{work_dir})"
+                #puts `git pull`
                 puts "rake #{work_dir}"
                 puts `rake`
                 puts "rake clobber"
