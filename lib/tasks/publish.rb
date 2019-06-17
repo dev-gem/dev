d1644ced7d0773cf6a8147a476c989395257386b
lou-parslow <lou.parslow@gmail.com>
2019-06-17 11:34:21 -0600

    'all'

--- lib/tasks/publish.rb
+++ lib/tasks/publish.rb
@@ -25,10 +25,5 @@ class Publish < Array
 				end
 			end
 		end
-
-		#Dir.glob("#{Rake.application.original_dir}/**/*.{nupkg,msi,gem}").each{|publish_file|
-		#	dest="#{Environment.default.publish_dir}/#{File.basename(publish_file)}"
-		#	add_quiet "<%FileUtils.cp('#{publish_file}','#{dest}')%>" if(!File.exists?(dest) && !publish_file.include?('packages/'))
-		#}
 	end
 end
--- lib/tasks/test.rb
+++ lib/tasks/test.rb
@@ -45,7 +45,9 @@ class Test < Array
 		end
 
 		# dotnet test
+		puts "scanning for **/*.Test.csproj" if Environment.default.debug?
 		Dir.glob('**/*.Test.csproj'){|proj|
+			puts "found #{proj}" if Environment.default.debug?
 			text = IO.read(proj)
 			if(text.include?('/netcoreapp'))
 				add_quiet("dotnet test #{proj}")
